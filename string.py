afde319b0a6c5773edb278410df76811eb476209
mdannikov <m.dannicov@mail.ru>
2021-06-09 10:35:15 +0300

    второй блок занятий

--- string.py
+++ string.py
@@ -8,19 +8,30 @@ print(s)"""
 """Кодирование осуществляется следующим образом:
 s = 'aaaabbсaa' преобразуется в 'a4b2с1a2', то есть группы одинаковых символов исходной строки заменяются на этот символ и количество его повторений в этой позиции строки."""
 
+#ggtyg
 a = str(input())
-
 i = 0
-q = 0
+q = 1
 l = len(a)
 while i <= l:
     for j in a:
-        b = a[i]
-        sum_b = a.count(b)
-        if b not in a[:i]:
-            print(b + str(sum_b))
-        i += 1
+        if a.find(a[i+1]):
+            c = a[i+1]
+            if j == c:
+                i += 1
+                q += i
+            elif j != c:
+                print(j + str(q))
+                q = 1
+                i += 1
+        else:
+            c = a[-1]
+            if j == c:
+                i = 0
+                i += 1
+                q += i
+            elif j != c:
+                print(j + str(q))
     else:
-
         break
 
